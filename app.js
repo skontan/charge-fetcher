f2786b678e95d16995154d3a5eeb3599aeca7265
Skontan <skontan@gmail.com>
2022-02-19 23:22:30 +0100

    First commit.

---
+++ app.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+const fetchChargeFinder =async () => {
+    const data = await axios.get("https://api.chargefinder.com/status/556gm2");
+    console.log(data.data);
+}
+
+fetchChargeFinder();
+
+setInterval(fetchChargeFinder, 1000 * 5)
