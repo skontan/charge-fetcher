e8d1375301129a339b95e53fbc39d3afd235ddc8
Skontan <skontan@gmail.com>
2022-02-20 11:29:35 +0100

    Move fetch into api.

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,17 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+const fetchChargeFinder = async () => {
+  const data = await axios.get("https://api.chargefinder.com/status/556gm2");
+  console.log(data.data);
+};
+
 app.get("/", (req: any, res: any) => {
+  setInterval(fetchChargeFinder, 1000 * 60);
+
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-const fetchChargeFinder = async () => {
-  const data = await axios.get("https://api.chargefinder.com/status/556gm2");
-  console.log(data.data);
-};
-
-fetchChargeFinder();
-
-setInterval(fetchChargeFinder, 1000 * 60);
