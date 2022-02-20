9ec95cb1606041423a225207ff87303eb0825da7
Skontan <skontan@gmail.com>
2022-02-20 01:21:58 +0100

    Add web server.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
-const axios = require('axios');
+const axios = require("axios");
+const http = require("http");
+
+const hostname = "https://charge-fetcher.onrender.com";
+const port = 3000;
+
+const server = http.createServer((req: any, res: any) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 const fetchChargeFinder = async () => {
-    const data = await axios.get("https://api.chargefinder.com/status/556gm2");
-    console.log(data.data);
-}
+  const data = await axios.get("https://api.chargefinder.com/status/556gm2");
+  console.log(data.data);
+};
 
 fetchChargeFinder();
 
-setInterval(fetchChargeFinder, 1000 * 60)
+setInterval(fetchChargeFinder, 1000 * 60);
